b0682f3a4a4eb2dfb8fdb2d18c381ddddb75550b
abidi <abidi@SAMET>
2024-12-04 20:08:02 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Hedef IP: ");
+        string target = Console.ReadLine();
+
+        Console.Write("Port: ");
+        int port = int.Parse(Console.ReadLine());
+
+        using (TcpClient tcpClient = new TcpClient())
+        {
+            try
+            {
+                tcpClient.Connect(target, port);
+                Console.WriteLine($"Port {port} açık.");
+            }
+            catch
+            {
+                Console.WriteLine($"Port {port} kapalı.");
+            }
+        }
+    }
+}
